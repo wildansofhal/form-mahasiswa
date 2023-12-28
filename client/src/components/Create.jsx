c8d4390042a8b2c67899785ba9cbc0752c23711d
wildansofhal <wildansofhal@gmail.com>
2023-12-28 14:15:27 +0700

    update

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -105,141 +105,145 @@ export default function Create() {
             borderRadius: "1rem",
           }}
         >
-        <Form method="POST">
-          <div className="form-input">
-            <label>Nama</label>
-            <input
-              name="nama"
-              type="text"
-              value={mahasiswa}
-              onInput={(e) => setMahasiswa(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-input">
-            <label>NIM</label>
-            <input
-              name="nim"
-              type="number"
-              value={nim}
-              onInput={(e) => setNim(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-input">
-            <label>Jurusan</label>
-            <input
-              name="jurusan"
-              type="text"
-              value={jurusan}
-              onInput={(e) => setJurusan(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-input">
-            <label>Fakultas</label>
-            <input
-              name="fakultas"
-              type="text"
-              value={fakultas}
-              onInput={(e) => setFakultas(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-input">
-            <label>Jenis Kelamin</label>
-            <select
-              name="jenisKelamin"
-              value={jenisKelamin}
-              onChange={(e) => setJenisKelamin(e.target.value)}
-              required
-            >
-              <option value="laki-laki">Laki-laki</option>
-              <option value="perempuan">Perempuan</option>
-            </select>
-          </div>
-          <div className="form-input">
-            <label>Tempat Lahir</label>
-            <input
-              name="tempatLahir"
-              type="text"
-              value={tempatLahir}
-              onInput={(e) => setTempatLahir(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-input">
-            <label>Tanggal Lahir</label>
-            <input
-              name="tanggalLahir"
-              type="date"
-              value={tanggalLahir}
-              onInput={(e) => setTanggalLahir(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-input">
-            <label>Nomor Telepon</label>
-            <input
-              name="noTelepon"
-              type="text"
-              value={noTelepon}
-              onInput={(e) => setNoTelepon(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-input">
-            <label>Alamat</label>
-            <textarea
-              name="alamat"
-              value={alamat}
-              onInput={(e) => setAlamat(e.target.value)}
-            ></textarea>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignContent: "center",
-              gap: "1rem",
-              marginTop: "2rem",
-            }}
-          >
-            <button type="submit">Simpan</button>
-          </div>
-        </Form>
-        </div>
-        
-        <h1 style={{ textAlign: "center", margin: "2rem 0" }}>
-          List Mahasiswa
-        </h1>
-        <div
-          style={{
-            borderRadius: "1rem",
-          }}
-        >
-          {mhs.map((mahasiswa) => (
-            <div key={mahasiswa._id}
+          <Form method="POST">
+            <div className="form-input">
+              <label>Nama</label>
+              <input
+                name="nama"
+                type="text"
+                value={mahasiswa}
+                onInput={(e) => setMahasiswa(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input">
+              <label>NIM</label>
+              <input
+                name="nim"
+                type="number"
+                value={nim}
+                onInput={(e) => setNim(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input">
+              <label>Jurusan</label>
+              <input
+                name="jurusan"
+                type="text"
+                value={jurusan}
+                onInput={(e) => setJurusan(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input">
+              <label>Fakultas</label>
+              <input
+                name="fakultas"
+                type="text"
+                value={fakultas}
+                onInput={(e) => setFakultas(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input">
+              <label>Jenis Kelamin</label>
+              <select
+                name="jenisKelamin"
+                value={jenisKelamin}
+                onChange={(e) => setJenisKelamin(e.target.value)}
+                required
+              >
+                <option value="laki-laki">Laki-laki</option>
+                <option value="perempuan">Perempuan</option>
+              </select>
+            </div>
+            <div className="form-input">
+              <label>Tempat Lahir</label>
+              <input
+                name="tempatLahir"
+                type="text"
+                value={tempatLahir}
+                onInput={(e) => setTempatLahir(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input">
+              <label>Tanggal Lahir</label>
+              <input
+                name="tanggalLahir"
+                type="date"
+                value={tanggalLahir}
+                onInput={(e) => setTanggalLahir(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input">
+              <label>Nomor Telepon</label>
+              <input
+                name="noTelepon"
+                type="text"
+                value={noTelepon}
+                onInput={(e) => setNoTelepon(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-input">
+              <label>Alamat</label>
+              <textarea
+                name="alamat"
+                value={alamat}
+                onInput={(e) => setAlamat(e.target.value)}
+              ></textarea>
+            </div>
+            <div
               style={{
-                margin: "0.5rem 0",
-                border: "0.25px solid white",
-                padding: "2rem",
-                borderRadius: "0.5rem",
                 display: "flex",
-                justifyContent: "space-between",
+                justifyContent: "center",
                 alignContent: "center",
+                gap: "1rem",
+                marginTop: "2rem",
               }}
             >
-              <div className="form-display">
-                <h2>{`${mahasiswa.nama}`}</h2>
-                <p>{`${mahasiswa.nim}`}</p>
-              </div>
-              <Link to={`/${mahasiswa._id}`} style={{ color: "white" }}>
-                <button>Detail</button>
-              </Link>
+              <button type="submit">Simpan</button>
             </div>
-          ))}
+          </Form>
         </div>
+        {mhs.length > 0 && (
+          <>
+            <h1 style={{ textAlign: "center", margin: "2rem 0" }}>
+              List Mahasiswa
+            </h1>
+            <div
+              style={{
+                borderRadius: "1rem",
+              }}
+            >
+              {mhs?.map((mahasiswa) => (
+                <div
+                  key={mahasiswa._id}
+                  style={{
+                    margin: "0.5rem 0",
+                    border: "0.25px solid white",
+                    padding: "2rem",
+                    borderRadius: "0.5rem",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignContent: "center",
+                  }}
+                >
+                  <div className="form-display">
+                    <h2>{`${mahasiswa.nama}`}</h2>
+                    <p>{`${mahasiswa.nim}`}</p>
+                  </div>
+                  <Link to={`/${mahasiswa._id}`} style={{ color: "white" }}>
+                    <button>Detail</button>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </>
   );
